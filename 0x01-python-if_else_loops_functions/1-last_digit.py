216725be680569b707ffbd7625215ddb38ba494c
jfbm74 <jfbm74@gmail.com>
2020-04-28 14:22:12 -0500

    Fixing PEP8 Style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,14 @@ import random
 
 number = random.randint(-10000, 10000)
 if (number < 0):
-    number = number * -1
-    x = (number % 10) * -1
+    x = abs(number) % 10
+    x = x * -1
 else:
     x = number % 10
-if (x > 5):
+if x > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".
           format(number, x))
-elif (x == 0):
+elif x == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, x))
 else:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
