02bd26ae2e6b381e075f3043328ba132ca983c5d
jfbm74 <jfbm74@gmail.com>
2020-04-27 19:13:37 -0500

    Adding task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,3 +4,5 @@ word = "Holberton"
 print("First 3 letters: {0:s}".format(word[0:3]))
 print("Last 2 letters: {0:s}".format(word[7:]))
 print("Middle word: {0:s}".format(word[1:8]))
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
