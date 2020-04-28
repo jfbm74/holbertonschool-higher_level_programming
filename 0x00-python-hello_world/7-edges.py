fce294dcb0c5805e19c69d483c543124b97e4849
jfbm74 <jfbm74@gmail.com>
2020-04-27 19:11:09 -0500

    Adding task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {0:s}".format(word[0:3]))
+print("Last 2 letters: {0:s}".format(word[7:]))
+print("Middle word: {0:s}".format(word[1:8]))
