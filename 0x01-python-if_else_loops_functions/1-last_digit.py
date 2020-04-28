fe13b2aa9bb4e19b5ba636f8fc2a57f051a6f162
jfbm74 <jfbm74@gmail.com>
2020-04-28 14:17:49 -0500

    Fixing Betty Style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-import math
+
 number = random.randint(-10000, 10000)
 if (number < 0):
     number = number * -1
@@ -8,9 +8,10 @@ if (number < 0):
 else:
     x = number % 10
 if (x > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, x))
+    print("Last digit of {:d} is {:d} and is greater than 5".
+          format(number, x))
 elif (x == 0):
-    print("Last digit of {} is {} and is 0".format(number, x))
+    print("Last digit of {:d} is {:d} and is 0".format(number, x))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0"
           .format(number, x))
