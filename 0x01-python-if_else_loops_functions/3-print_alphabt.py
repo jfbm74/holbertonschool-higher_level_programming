b024910cb6e13676cbc3c374bfd178b7ce8e9adc
jfbm74 <jfbm74@gmail.com>
2020-04-28 15:15:54 -0500

    Adding Task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(97, 101):
+    tmp = chr(i)
+    print("{:s}".format(tmp), end='')
+for i in range(102, 113):
+    tmp = chr(i)
+    print("{:s}".format(tmp), end='')
+for i in range(114, 123):
+    tmp = chr(i)
+    print("{:s}".format(tmp), end='')
