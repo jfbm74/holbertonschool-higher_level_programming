a1c435721c9732d7c6d6f2c9a56120e6355d4af6
jfbm74 <jfbm74@gmail.com>
2020-04-28 13:37:33 -0500

    Adding Task 0 & 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+import math
+number = random.randint(-10000, 10000)
+if (number < 0):
+    number = number * -1
+    x = (number % 10) * -1
+else:
+    x = number % 10
+if (x > 5):
+    print("Last digit of {} is {} and is greater than 5".format(number, x))
+elif (x == 0):
+    print("Last digit of {} is {} and is 0".format(number, x))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, x))
