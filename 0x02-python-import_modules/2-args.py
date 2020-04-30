3e85699e4bb239e6a972644c75c48c21dff15347
jfbm74 <jfbm74@gmail.com>
2020-04-30 17:20:02 -0500

    Adding Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
+    import sys
+
     argc = len(sys.argv)
     if argc == 1:
-        print("{} arguments.".format(argc - 1))
+        print("0 arguments.")
+        exit()
     else:
-        print("{} argument:".format(argc - 1))
+        print("{} argument{}:".format(argc - 1, '' if argc == 2 else 's'))
         for i in range(1, argc):
             print("{}: {}".format(i, sys.argv[i]))
