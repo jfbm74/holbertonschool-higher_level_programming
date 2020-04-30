958a4ad903f4d766936c911a210e2181bcdcad6b
jfbm74 <jfbm74@gmail.com>
2020-04-29 22:23:06 -0500

    Adding task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -35,12 +35,13 @@ listint_t *insert_node(listint_t **head, int number)
 			pivot = pivot->next;
 			if (new->n < pivot->n)
 			{
-			new->next = current->next;
-			current->next = new;
-			return (new);
+				new->next = current->next;
+				current->next = new;
+				return (new);
 			}
 			current = current->next;
 		}
 	}
+	current->next = new;
 	return (new);
 }
