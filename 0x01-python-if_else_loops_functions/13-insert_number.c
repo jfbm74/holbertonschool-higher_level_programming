a4736518c6949762a33f08ad07fba956f537b3d2
jfbm74 <jfbm74@gmail.com>
2020-04-29 22:13:36 -0500

    Adding task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_node - Return new node
+ * @head: Address of first node
+ * @number: Value of new node
+ * Return: New node if exits
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+	listint_t *pivot;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
+	new->n = number;
+	current = *head;
+	pivot = *head;
+	new->next = NULL;
+	if (new->n < pivot->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		while (pivot->next != NULL)
+		{
+			pivot = pivot->next;
+			if (new->n < pivot->n)
+			{
+			new->next = current->next;
+			current->next = new;
+			return (new);
+			}
+			current = current->next;
+		}
+	}
+	return (new);
+}
