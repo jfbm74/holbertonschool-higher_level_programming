6cc7bdfad89733829f0d0b04e688a0ff7e12a100
jfbm74 <jfbm74@gmail.com>
2020-05-05 12:21:14 -0500

    Updating All tasks

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for x in range(len(my_list)):
-        print("{}".format(my_list[x]))
+    print(*my_list)
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_cpylist = my_list.copy()
+    if len(my_list) < idx or idx < 0:
+        return my_cpylist
+    else:
+        my_cpylist[idx] =  element
+        return my_cpylist
