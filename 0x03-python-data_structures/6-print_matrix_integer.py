3fd80c10e8f117ac7a5cb3ca4ac8408d1deb2183
jfbm74 <jfbm74@gmail.com>
2020-05-05 16:57:11 -0500

    Function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix[0])):
-        for col in range(len(matrix[0])):
-            print("{:d}".format(matrix[row][col]), end=" ")
-        print()
+    if matrix == [[]]:
+        print("")   
+    else:
+        for row in range(len(matrix[0])):
+            for col in range(len(matrix[0])):
+                print("{:d}".format(matrix[row][col]), end=" ")
+            print()
