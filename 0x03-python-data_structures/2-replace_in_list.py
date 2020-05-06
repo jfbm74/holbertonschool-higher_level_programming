a3c58badcfe1fa94e0633c6d2be858d8b8405128
jfbm74 <jfbm74@gmail.com>
2020-05-06 11:43:14 -0500

    Adding task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,8 @@
+ #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if len(my_list) < idx or idx < 0:
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
