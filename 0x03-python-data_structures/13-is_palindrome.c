245820cea2a45cf05b268921671e145906cc8688
jfbm74 <jfbm74@gmail.com>
2020-05-05 21:02:12 -0500

    Return: 0 if it is not a palindrome, 1 if it is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * is_palindrome - function that checks if a singly linked list is a palindrome
+ * @head: first node of the list
+ * Return: Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+	int len = 1;
+	int i, j = 0;
+	listint_t *pivot = NULL;
+
+	if (*head == NULL || head == NULL)
+		return (1);
+
+	pivot = *head;
+	while (pivot->next != NULL)
+	{
+		len++;
+		pivot = pivot->next;
+	}
+	int array[len];
+
+	pivot = *head;
+	for (i = 0; i < len; i++)
+	{
+		array[i] = pivot->n;
+		pivot = pivot->next;
+	}
+	for (i = 0, j = (len - 1); i < j; i++, j--)
+	{
+		if (array[i] != array[j])
+			return (0);
+	}
+	return (1);
+}
