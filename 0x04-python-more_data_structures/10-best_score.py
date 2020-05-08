c7874e80678a454e6410af6b2d14ca6d71422393
jfbm74 <jfbm74@gmail.com>
2020-05-07 23:36:00 -0500

    Adding task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    score = -9999999999999999999
+    besty = ""
+    if a_dictionary is not None:
+        for key, value in a_dictionary.items():
+            if value > score:
+                score = value
+                besty = key
+        return besty
+    else:
+        return None
