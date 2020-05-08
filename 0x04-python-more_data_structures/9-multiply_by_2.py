40c7aefe87578c98b7ac371b05202d768ad29b6f
jfbm74 <jfbm74@gmail.com>
2020-05-07 22:52:36 -0500

    Adding Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
     for key, value in new_dic.items():
-        new_dic[key] = value * 2 
+        new_dic[key] = value * 2
     return new_dic
