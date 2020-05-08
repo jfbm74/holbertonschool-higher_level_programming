b77282eb91543b529ec17d620d86fe33977f56f3
jfbm74 <jfbm74@gmail.com>
2020-05-07 23:36:39 -0500

    Adding task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    score = -9999999999999999999
+    score = -99999999999999999999999999999999
     besty = ""
     if a_dictionary is not None:
         for key, value in a_dictionary.items():
