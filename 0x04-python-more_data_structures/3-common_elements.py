cdbbf5042b7218f5911c70f7425c4999bc6cae07
jfbm74 <jfbm74@gmail.com>
2020-05-07 20:22:38 -0500

    Adding Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = []
 
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                set_3.append(i)
+    s1 = set(set_1)
+    s2 = set(set_2)
+    set_3 = [s1 & s2]
 
     return set_3
