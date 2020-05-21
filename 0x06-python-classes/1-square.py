88e4bf2b437e8bbee82969784194b0f33443bc76
jfbm74 <jfbm74@gmail.com>
2020-05-21 12:11:37 -0500

    Adding task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this module create the class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """
+        __init__ Square's Instantiation with size (no type/value verification)
+        Args:
+            size (): size of the square
+        """
+        self.__size = size+
