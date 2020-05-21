d432f208fa7b47150d5fbc61765bb6ec62927739
jfbm74 <jfbm74@gmail.com>
2020-05-21 17:50:12 -0500

    Adding task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" this module create the class Square"""
+
+
+class Square:
+    """ Class Square with constuctor method"""
+    def __init__(self, size=0):
+        """Initializes the class Square
+        Arg
+            size: size to initializate __size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of a square
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter function of size
+        Returns:
+            size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        """
+        setter function of attribute size
+        Args:
+            value: value for __size
+        """
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        if val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #:
+            if size is equal to 0, print an empty line
+        Returns: Nothing
+
+        """
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
