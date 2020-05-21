f6a5425be15612d7eee9fccd9e9d464780fcb07c
jfbm74 <jfbm74@gmail.com>
2020-05-21 13:51:01 -0500

    Adding task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" this module create the class Square"""
+
+
+class Square:
+    """ Class Square constuctor method"""
+    def __init__(self, size=0):
+        """
+        Initializes with size (no type/value verification)
+        Arg
+            size: size of the square
+        """
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates square area
+        Returns: area
+
+        """
+        return self.__size ** 2
