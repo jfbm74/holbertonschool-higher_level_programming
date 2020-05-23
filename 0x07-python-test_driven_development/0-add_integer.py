3aaee02bcd429bff7d8967d3f4fa12fb34808f26
jfbm74 <jfbm74@gmail.com>
2020-05-23 12:11:04 -0500

    Adding add integer function and test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/python3
+"""
+Module add_integer
+module to add two integer
+Return: a + b
+"""
+
+
+def add_integer(a, b=98):
+    """this functions return a add b
+    and return error if number is a string
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
