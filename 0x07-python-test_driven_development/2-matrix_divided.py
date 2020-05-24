3c6d196bd02dabddee077f763f3b149bd7f47f48
jfbm74 <jfbm74@gmail.com>
2020-05-24 10:39:13 -0500

    Adding function that divides a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module matrix_divided
+module divide a matrix into given divisor
+Return: new matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix.
+    Args:
+        matrix: must be a list of lists of integers or floats
+        div: number to divide each element of a matrix
+
+    Returns: New matrix
+
+    """
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+
+    rows = len(matrix)
+    columns = len(matrix[0])
+    new_matrix = [[0 for i in range(columns)] for i in range(rows)]
+
+    for i in range(rows):
+        if (len(matrix[i])) != columns:
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in range(columns):
+            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+            new_matrix[i][j] = round((matrix[i][j] / div), 2)
+    return new_matrix
