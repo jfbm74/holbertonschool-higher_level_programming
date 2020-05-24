74a966a2aaf9731ec530e029ba569c83a964d574
jfbm74 <jfbm74@gmail.com>
2020-05-24 13:44:52 -0500

    Adding function that prints My name is ...

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module say_my_name
+module that prints  My name is <first name> <last name>
+Return: Nothing
+"""
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints  My name is <first name> <last name>
+    Args:
+        first_name: First name given to function
+        last_name: Last name given to function
+
+    Returns: Nothing
+
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        print("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
+
