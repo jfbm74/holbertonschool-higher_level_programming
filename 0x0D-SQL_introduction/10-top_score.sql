32af3add16930c56741f035474fe8b63fc53cf1a
jfbm74 <jfbm74@gmail.com>
2020-07-13 14:01:28 -0500

    Adding Task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all records of the table second_table of the
 
-SELECT score, name from second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
