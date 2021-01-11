490a083371c7a85aab35091e816da3b71a352fb0
jfbm74 <jfbm74@gmail.com>
2021-01-11 12:57:20 -0500

    Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,13 +5,11 @@
  a parameter
 """
 
-
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
     url = argv[1]
-    email ={'email': argv[2]}
+    email = {'email': argv[2]}
     req = requests.post(url, email)
     print(req.text)
