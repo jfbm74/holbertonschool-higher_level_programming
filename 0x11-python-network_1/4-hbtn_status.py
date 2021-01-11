9e3e18433b005eeba99d855d3af15c10e1836daa
jfbm74 <jfbm74@gmail.com>
2021-01-11 12:37:25 -0500

    Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    data = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(data.text)))
+    print('\t- content: {}'.format(data.text))
