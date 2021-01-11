5ed0e17171fdaf870180f37c3f0fdeab012c7c27
jfbm74 <jfbm74@gmail.com>
2021-01-11 12:31:21 -0500

    Fixing Style issue

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL 
-and displays the body of the response (decoded in utf-8).
+takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameteryy
 """
 
 from urllib import request, error
 from sys import argv
 
 if __name__ == '__main__':
-    url = argv[1]
+    URL = argv[1]
     try:
-        with request.urlopen(url) as response:
+        with request.urlopen(URL) as response:
             html = response.read()
             print(html.decode('utf-8'))
     except error.URLError as e:
-        print('Error code: {}'.format(e.code))        
+        print('Error code: {}'.format(e.code))
