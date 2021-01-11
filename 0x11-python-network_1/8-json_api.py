04bc40e2850a60a01d13a5012bb3e43044d53b11
jfbm74 <jfbm74@gmail.com>
2021-01-11 14:18:19 -0500

    Python script that sends a letter ito POST

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    if len(argv) > 1:
+        query = {'q': argv[1]}
+    else:
+        query = {'q': ""}
+    answer = requests.post('http://0.0.0.0:5000/search_user', data)
+    try:
+        json = answer.json()
+        if json:
+            print("[{}] {}".format(json['id'],json['name']))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
